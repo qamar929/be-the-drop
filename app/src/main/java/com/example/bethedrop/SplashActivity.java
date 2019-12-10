a2458545e5bf5a781165827cd4c984139b883bb5
Qamar <syedqamarabbas949@gmail.com>
2019-12-10 16:09:21 +0500

    Be the Drop version 1.2

---
+++ app/src/main/java/com/example/bethedrop/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.example.bethedrop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(5*1000);
+
+                    // After 5 seconds redirect to another intent
+                    Intent intent = new Intent(SplashActivity.this,DonorHomeActivity.class);
+                    startActivity(intent);
+
+                    //Remove activity
+                    finish();
+                } catch (Exception e) {
+                }
+            }
+        };
+        // start thread
+        background.start();
+
+
+    }
+}
